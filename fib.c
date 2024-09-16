b0285f09289b5d34b4b3ded1b9c013c1d31026db
TylerLN <tylerlngu@csu.fullerton.edu>
2024-09-15 22:18:06 -0700

    Largest Integer with unsigned long long

--- fib.c
+++ fib.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-   printf("Hello!\n");
+unsigned long long int FibIterative(int n) {
+   if (n == 1) {
+      return 0;
+   } else if (n == 2) {
+      return 1;
+   }
+   unsigned long long int first = 0, second = 1, next_value;
+   for (int i = 3; i <= n; i++) {
+      next_value = first + second;
+      first = second;
+      second = next_value;
+   }
+   return second;
+}
+
+unsigned long long int FibRecursive(int n) {
+   if(n == 1){
+      return 0;
+   } else if (n == 2) {
+      return 1;
+   } else {
+      
+      return (FibRecursive(n-1) + FibRecursive(n-2));
+   }
+}
+
+int main(int argc, char *argv[]) {
+   int value = atoi(argv[1]);
+   FILE *text_file = fopen(argv[3], "r");
+   int file_value;
+   fscanf(text_file, "%d", &file_value);
+   fclose(text_file);
+
+   int N = value + file_value;
+
+   if (argv[2][0] == 'i') {
+     unsigned long long int result = FibIterative(N);
+      printf("%llu", result);
+   } else if (argv[2][0] == 'r') {
+      unsigned long long int result = FibRecursive(N);
+      printf("%llu", result);
+   }
    return 0;
+
 }
