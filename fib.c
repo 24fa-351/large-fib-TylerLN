249d8633c5cc4e1159f6f4b224bf0be71b549ec5
TylerLN <tylerlngu@csu.fullerton.edu>
2024-11-20 00:21:42 -0800

    FUnction names

--- fib.c
+++ fib.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned long long int OverFlow(unsigned long long int current,
+unsigned long long int overflow(unsigned long long int current,
                                 unsigned long long int next) {
   return (current > ULLONG_MAX - next);
 }
 
-unsigned long long int FibIterative(int fib_index) {
+unsigned long long int fib_iterative(int fib_index) {
   if (fib_index == 1) {
     return 0;
   } else if (fib_index == 2) {
@@ -19,7 +19,7 @@ unsigned long long int FibIterative(int fib_index) {
   unsigned long long int next_value;
 
   for (int ix = 3; ix <= fib_index; ix++) {
-    if (OverFlow(first, second)) {
+    if (overflow(first, second)) {
       printf("Overflow at %d\n", ix);
       return 0;
     }
@@ -27,35 +27,43 @@ unsigned long long int FibIterative(int fib_index) {
     first = second;
     second = next_value;
   }
+
   return second;
 }
 
-unsigned long long int FibRecursive(int recursive_index) {
+unsigned long long int fib_recursive(int recursive_index) {
   if (recursive_index == 1) {
     return 0;
   } else if (recursive_index == 2) {
     return 1;
   }
 
-  unsigned long long int current = FibRecursive(recursive_index - 1);
-  unsigned long long int next = FibRecursive(recursive_index - 2);
+  unsigned long long int current = fib_recursive(recursive_index - 1);
+  unsigned long long int next = fib_recursive(recursive_index - 2);
 
-  if (OverFlow(current, next)) {
+  if (overflow(current, next)) {
     printf("Overflow occured at index %d\n", recursive_index);
     return 0;
   }
+
   return current + next;
 }
 
 int main(int argc, char *argv[]) {
+  if (argc != 3) {
+    printf("Usage: ./fib <number> <i/r>\n");
+    return 1;
+  }
+
   int user_input = atoi(argv[1]);
 
   if (argv[2][0] == 'i') {
-    unsigned long long int result = FibIterative(user_input);
+    unsigned long long int result = fib_iterative(user_input);
     printf("%llu", result);
   } else if (argv[2][0] == 'r') {
-    unsigned long long int result = FibRecursive(user_input);
+    unsigned long long int result = fib_recursive(user_input);
     printf("%llu", result);
   }
+
   return 0;
 }
